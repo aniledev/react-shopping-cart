ab76ddfaa6ce3fafcbf96bc83b38250dd5afaebb
aniledev <elinamcgill@gmail.com>
2021-01-04 14:51:34 -0500

    REFRACTOR: move code into CartList component

--- src/Components/App.js
+++ src/Components/App.js
@@ -80,26 +80,6 @@ export default class App extends Component {
       );
     });
 
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + "-" + idx;
-      const selectedOption = this.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
-
     return (
       <div className="App">
         <Header />
@@ -112,17 +92,7 @@ export default class App extends Component {
               this.updateFeature(feature, newValue)
             }
           />
-          {/* CartList component */}
-          <section className="main__summary">
-            <h2>Your cart</h2>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
-          </section>
+          <CartList selected={this.state.selected} />
         </main>
       </div>
     );
