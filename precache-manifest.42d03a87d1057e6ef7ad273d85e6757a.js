2b55fab5e90d72280c6cb417d8b0c9eda7a045c1
aniledev <elinamcgill@gmail.com>
2021-01-04 17:30:35 -0500

    Updates

---
+++ precache-manifest.42d03a87d1057e6ef7ad273d85e6757a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be36d1896952f617a6bac9f65229f1f9",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "644fa57d33ee93a77a2e",
+    "url": "/react-shopping-cart/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/react-shopping-cart/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "644fa57d33ee93a77a2e",
+    "url": "/react-shopping-cart/static/js/main.de80a179.chunk.js"
+  },
+  {
+    "revision": "89264515d9191e288e96",
+    "url": "/react-shopping-cart/static/js/runtime~main.b0014d54.js"
+  }
+]);
